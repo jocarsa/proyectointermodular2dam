e02bd345f925e48fd8d2a25c2a257f85f5d4fab0
Jose Vicente Carratala <info@josevicentecarratala.com>
2025-11-21 17:42:08 +0100

    1

---
+++ Sistemas de gestión empresarial/002-Mi primer servidor en node/002-un poco verboso.js
@@ -0,0 +1,16 @@
+// Primero incluyo la librería http
+var servidor = require('http');
+
+// Ahora creo un servidor
+servidor.createServer(function(req,res){
+  // Envío las cabeceras 
+  // 200 es ok
+  // Voy a devolver texto en formato HTML
+  res.writeHead(200,{'Content-Type':'text/html'})
+  // Escribo un mensaje en pantalla
+  res.end("Hola mundo desde Node.js")
+  // El servidor un poco verboso
+  console.log("Tranquilo que he arrancado...");
+}).listen(3000) // Escucho en el puerto 3000
+
+
